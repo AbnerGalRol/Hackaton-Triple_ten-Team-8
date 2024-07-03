6d79c58c3722d4618992676257ed9058eedad6e4
Umiterr <37944964+Umiterr@users.noreply.github.com>
2024-07-03 00:41:11 -0600

    add tailwind  + hotfix

---
+++ frontend/hackaton-webpage/src/commponents/LandingPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import header from "../assets/header.png";
+
+export default function LandingPage() {
+  return (
+    <img src={header} className="absolute inset-0 w-full h-full object-cover" />
+  );
+}
